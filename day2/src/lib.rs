1b3bb1897b84fc0c7f681481c8f50c86611aca47
Markus Ineichen <kusi.ineichen@gmail.com>
2020-12-03 21:42:16 +0100

    Reuse linebuffer

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,7 +1,6 @@
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
-use core::str::FromStr;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -24,40 +23,49 @@ fn split_once(input: &str, delimiter: char) -> Result<(&str, &str), Error> {
 }
 
 pub struct FileReaderIterator {
-    lines: std::io::Lines<BufReader<File>>
+    lines: BufReader<File>,
+    buffer: String
+}
+
+fn parse_line(input: &str) -> Result<(OccurenceRule, String), Error> {
+    let (min_text, rest) = split_once(input, '-')?;
+    let (max_text, rest) = split_once(rest, ' ')?;                    
+
+    let min = min_text.parse()?;
+    let max = max_text.parse()?;
+    let mut chars = rest.char_indices();
+    let (_, char) = chars.next().ok_or(Error::Format("No char provided in role"))?;
+    let rule = OccurenceRule { first: min, second: max, char };
+
+    let (offset, _) = chars.skip(2).next().ok_or(Error::Format("No text provided in role"))?;
+    let text = rest.split_at(offset).1;
+    // Try to get rid of more
+    Ok((rule, text.to_string()))
 }
 
 impl Iterator for FileReaderIterator {
     type Item = Result<(OccurenceRule, String), Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.lines.next().map(|s| {
-            match s {
-                Ok(line) =>  {
-                    let (min_text, rest) = split_once(&line, '-')?;
-                    let (max_text, rest) = split_once(rest, ' ')?;                    
-
-                    let min = min_text.parse()?;
-                    let max = max_text.parse()?;
-                    let mut chars = rest.char_indices();
-                    let (_, char) = chars.next().ok_or(Error::Format("No char provided in role"))?;
-                    let rule = OccurenceRule { first: min, second: max, char };
-
-                    let (offset, _) = chars.skip(2).next().ok_or(Error::Format("No text provided in role"))?;
-                    let text = rest.split_at(offset).1;
-                    // Try to get rid of more
-                    Ok((rule, text.to_string()))
-                },
-                Err(e) => Err(e.into())          
-            }    
-        })  
+        self.buffer.clear();
+        match self.lines.read_line(&mut self.buffer) {
+            Ok(n) if n > 0 =>  {
+                Some(parse_line(&self.buffer))                
+            },
+            Ok(_) => {
+                None
+            }
+            Err(e) => {
+                Some(Err(e.into()))
+            }
+        }
     }
 }
 
 pub fn load_rules() -> impl Iterator<Item=Result<(OccurenceRule, String), Error>> {
     let f = File::open("day2/input.txt").unwrap();
     let f = BufReader::new(f);
-    FileReaderIterator { lines: f.lines() }
+    FileReaderIterator { lines: f, buffer: String::new() }
 }
 
 #[derive(Debug)]
