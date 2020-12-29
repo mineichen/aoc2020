57f93c071293c7d10a764a8188ad7ec249dc08ee
Markus Ineichen <kusi.ineichen@gmail.com>
2020-12-29 09:00:30 +0100

    Split number collection into separate method

--- day23/src/lib.rs
+++ day23/src/lib.rs
@@ -1,45 +1,48 @@
 pub struct Game {
-    input: Vec<usize>
+    input: Vec<usize>,
+    current_cup_index: usize
 }
 impl Game {
     pub const fn new(input: Vec<usize>) -> Self {
-        Self { input }
+        Self { input, current_cup_index: 0 }
     }
-    pub fn solve(&mut self, rounds: usize) -> usize {
-        let mut current_cup_index = 0;
+    pub fn solve(&mut self, rounds: usize) {
         for _round in 0..rounds {
-            let current_cup_label = self.input[current_cup_index];
+            let current_cup_label = self.input[self.current_cup_index];
             let mut source_idx = WrappingCounter::new(
-                current_cup_index,
+                self.current_cup_index,
                 self.input.len()
             );
             let mut destination_idx = source_idx.clone();
 
             let next_current_cup = source_idx.next();
             let following_labels = [ 
-                self.input[next_current_cup as usize], 
-                self.input[source_idx.next() as usize],
-                self.input[source_idx.next() as usize]
+                self.input[next_current_cup], 
+                self.input[source_idx.next()],
+                self.input[source_idx.next()]
             ];
             let destination_label = self.decrease_label_until_none_match(current_cup_label, &following_labels);
             // println!("Round {}: {:?}, dest: {}, labels: {:?} ", round, self.input, destination_label, following_labels);
             loop {
-                let item = self.input[source_idx.next() as usize];
-                self.input[destination_idx.next() as usize] = item;
+                let item = self.input[source_idx.next()];
+                self.input[destination_idx.next()] = item;
                 if item == destination_label {
                     break;
                 }
             }
-            self.input[destination_idx.next() as usize] = following_labels[0];
-            self.input[destination_idx.next() as usize] = following_labels[1];
-            self.input[destination_idx.next() as usize] = following_labels[2];
-            current_cup_index = next_current_cup as usize;
+            self.input[destination_idx.next()] = following_labels[0];
+            self.input[destination_idx.next()] = following_labels[1];
+            self.input[destination_idx.next()] = following_labels[2];
+            self.current_cup_index = next_current_cup;
         }
+        
+    }
+    pub fn collect_from_label_1(&self) -> usize {
         let mut ctr = WrappingCounter::new(0, self.input.len());
-        while self.input[ctr.cur as usize] != 1 {
+        while self.input[ctr.cur] != 1 {
             ctr.next();
         }
-        (0..8).fold(0, |acc, _| acc * 10 + self.input[ctr.next() as usize] as usize)
+        (0..8).fold(0, |acc, _| acc * 10 + self.input[ctr.next()])
     }
     fn decrease_label_until_none_match(&self, label: usize, next_labels: &[usize]) -> usize {
         let mut result = label;
@@ -49,8 +52,7 @@ impl Game {
                 return result;
             }
         }
-    }
-    
+    }    
 }
 
 #[derive(Clone)]
@@ -73,12 +75,13 @@ mod tests {
     use {super::*};
     #[test]
     fn test() {
-        let mut game = Game {input: vec!(3, 8, 9, 1, 2, 5, 4, 6, 7)};
-        assert_eq!(92658374, game.solve(10));        
+        let mut game = Game::new(vec!(3, 8, 9, 1, 2, 5, 4, 6, 7));
+        game.solve(10);
+        assert_eq!(92658374, game.collect_from_label_1());        
     }
     #[test]
     fn test_decrease_label() {
-        let game = Game { input: vec!(1,2,3,4,5,6) };
+        let game = Game::new(vec!(1,2,3,4,5,6));
         assert_eq!(6, game.decrease_label_until_none_match(1, &[2,3,4]));
         assert_eq!(5, game.decrease_label_until_none_match(6, &[2,3,4]));
         assert_eq!(1, game.decrease_label_until_none_match(3, &[2,3,4]));
--- day23/src/part1.rs
+++ day23/src/part1.rs
@@ -2,5 +2,6 @@ pub mod lib;
 
 fn main() {
     let mut game = lib::Game::new(vec!(1,3,7,8,2,6,4,9,5));
-    println!("Game endstate: {}", game.solve(100));
+    game.solve(100);
+    println!("Game endstate: {}", game.collect_from_label_1());
 }
